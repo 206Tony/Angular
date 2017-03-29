81e267a636fa6229bf4e127285231216b6dc9f30
anthony zardis <your>
2017-03-29 13:16:08 -0700

    Angular-checkpoint-3

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,1 +1,20 @@
-angular.module('blocJams', []);
+function config($stateProvider, $locationProvider) {
+    $locationProvider
+        .html5Mode({
+            enabled: true,
+            requireBase: false
+    });
+    
+    $stateProvider
+        .state('landing',{
+            url: '/', 
+            templateUrl: '/templates/landing.html'
+    });
+    })
+    
+    .state('album', {
+        url: '/album',
+        templateUrl: '/templates/album.html'
+    });
+
+angular.module('blocJams', ['ui-router']);
